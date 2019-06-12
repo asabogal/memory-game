affc4004fcace73bd631bdeb8c488b61d99ba45f
asabogal <asabogal.git@gmail.com>
2019-06-12 14:08:56 -0400

    refactor flip logic

--- game.js
+++ game.js
@@ -13,21 +13,32 @@ function flipCard() {
     //set the variable to true to keep track of this flipped card
     hasFlippedCard = true
     firstCard = this
-  } else {
+    return; 
+  };
+
     //this will be the second clicked card
     hasFlippedCard = false;
     secondCard = this
-  }
+
+  checkMatch()
+};
 
   //add matching logic
   //fliped card must have the same data-name attribute to match
-  if (firstCard.dataset.name === secondCard.dataset.name) {
-    firstCard.removeEventListner('click', flipCard)
-    secondCard.removeEventListner('click', flipCard)
-  } else {
-    setTimeout(()=>{
-      firstCard.classList.remove('flip')
-      secondCard.classList.remove('flip')
-    }, 1500);
-  }
+function checkMatch() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name 
+  console.log(secondCard)
+  isMatch ? disableCards() : unflipCards()
+};
+
+function disableCards() {
+  firstCard.removeEventListner('click', flipCard)
+  secondCard.removeEventListner('click', flipCard)
+};
+
+function unflipCards() {
+  setTimeout(()=>{
+    firstCard.classList.remove('flip')
+    secondCard.classList.remove('flip')
+  }, 1500);
 };
